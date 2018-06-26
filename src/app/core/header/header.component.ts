278c76bf57e30ef125ffe6e092ccfc20c301587c
michal <marelix2@gmail.com>
2018-06-26 20:56:28 +0200

    half way of creating sound player

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,32 +1,53 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterContentInit, Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {SearchService} from "../../shared/services/search.service";
 import {LoginService} from "../../shared/services/login.service";
 import {Router} from "@angular/router";
+import {DisplayPodcastService} from "../../shared/services/display-podcast.service";
+import {Observable} from "rxjs/Observable";
+import {map, startWith} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterContentInit {
 
+  myControl: FormControl = new FormControl();
   searchForm: FormGroup;
   podcasts: SampleNamesModel[];
   user: UserModel;
+  tracks: playlistTracks[];
+
+  filteredOptions: Observable<string[]>;
+  treackAuto: string[];
+
 
   constructor(private formBuilder: FormBuilder,
               private searchService: SearchService,
               private loginService: LoginService,
-              private router: Router) {
+              private router: Router,
+              private displayPodcastService: DisplayPodcastService) {
   }
-
   ngOnInit() {
     this.loadUser();
     this.searchForm = this.searchFormBuild();
+    this.tracks = new Array<playlistTracks>();
     this.loadPodcasts();
 
+
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(val => this.filter(val)));
   }
+
+
+  filter(val: string): string[] {
+    return this.treackAuto.filter(option => option.toLowerCase().indexOf(val.toLowerCase()) === 0);
+  }
+
 
   searchFormBuild(): FormGroup {
     return this.formBuilder.group({
@@ -36,14 +57,25 @@ export class HeaderComponent implements OnInit {
 
   loadUser() {
 
-   this.user =  JSON.parse(localStorage.getItem('user'));
+    this.user = JSON.parse(localStorage.getItem('user'));
   }
 
   loadPodcasts() {
 
-    this.searchService.loadPodcasts().subscribe(w => {
-      this.podcasts = w;
+    this.displayPodcastService.loadPlaylists().subscribe(ref => {
+      this.tracks = ref;
+      this.loadOptions();
     });
+
+  }
+
+  loadOptions(){
+
+    this.treackAuto = this.tracks.map(ref => {
+      return ref.title;
+
+    });
+
   }
 
 
@@ -52,5 +84,12 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['/login']);
 
   }
+
+  ngAfterContentInit(): void {
+
+  }
+
+
+
 
 }
