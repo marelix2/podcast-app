550af0e1c4aa6d658422910bbb6830671dc15f9a
michal <marelix2@gmail.com>
2018-06-30 22:09:52 +0200

    addind playlist service

---
+++ src/app/shared/services/add-playlist.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from "angularfire2/firestore";
+
+@Injectable()
+export class AddPlaylistService {
+
+  playlistCollcetion: AngularFirestoreCollection<UserPlaylistModel>
+  playlistDocument : AngularFirestoreDocument<UserPlaylistModel>
+
+  constructor(private afs: AngularFirestore) {
+    this.playlistCollcetion = this.afs.collection('userPlaylist');
+  }
+
+
+  addPlaylist(playlist: UserPlaylistModel) {
+    this.playlistCollcetion.add(playlist).then( r =>{
+  }
+
+
+}
